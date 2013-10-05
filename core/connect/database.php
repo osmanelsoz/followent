1da8b09fc6dfde45f302c217dfd4d466c06b18c3
mbilgilioglu <memet_bilgilioglu@hotmail.com>
2013-10-05 21:48:19 +0300

    tasarım denemeleri
    database dosyaları ve yönetim dosyaları için benim dosyalarımı update
    etmeyin! css ve index sayfalarını düzenliyorum.

--- core/connect/database.php
+++ core/connect/database.php
@@ -7,8 +7,13 @@ $config = array(
 	'dbname' 	=> 'followent'
 );
 #connecting to the database by supplying required parameters
+try
+{
 $db = new PDO('mysql:host=' . $config['host'] . ';dbname=' . $config['dbname'], $config['username'], $config['password']);
- 
+ }
+catch (Exception $e) {
+    echo 'Caught exception: '.$e->getMessage()."\n";
+	}
 #Setting the error mode of our db object, which is very important for debugging.
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
--- index.php
+++ index.php
@@ -1,16 +1,41 @@
 <?php 
 #including our init.php
-require 'core/init.php';
+//require 'core/init.php';
 ?>
 <!doctype html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<link rel="stylesheet" type="text/css" href="css/style.css" >
-	<title>Login and registration</title>
+	<title>FOLLOWENT</title>
 </head>
 <body>	
-	<div id="container">
+<div id="main">
+	<div id="banner"> 
+		<div class="bannerAdi">FOLLOWENT </div>
+		<div class="bannerLinkler"><div class="bannerLink">Etkinlikler</div><div class="bannerLink">Gruplar</div><div class="bannerLink">Diğer</div> Giriş Yap</div>
+	</div>
+	<div id="icerik">
+		<div id="icerikLinkler"> <div class="icerikLinkBaslik"><b>Etkinlikler</b></div>
+			<div class="icerikLinkDetay">
+				Eğitim<br/>
+				Eğlence<br/>
+				Toplantı<br/>
+				Tanıtım<br/>
+				Proje <br/>
+			</div>
+			 <div class="icerikLinkBaslik"><b>Gruplar</b></div>
+			<div class="icerikLinkDetay">
+				IEEE<br/>
+				Spor Klubü<br/>
+				Doğa Klubü<br/>
+				Zeka Oyunları Klubü<br/>
+				Ne Klubü <br/>
+			</div>
+		</div>
+		<div id="icerikEtkinlikler"> etkinlik
+		
+			<div id="container">
 		<ul>
 			<li><a href="index.php">Home</a></li>
 			<li><a href="register.php">Register</a></li>
@@ -19,5 +44,9 @@ require 'core/init.php';
 		</ul>
 		<h1>Welcome to our site!</h1>
 	</div>
+		</div>
+	</div>
+	<div id="alt"> Site Bilgileri , hakkkımızda vs gibi kapanış bilgileri</div>
+</div>
 </body>
 </html>

9a7ef9dd4bd2e3e108847c0bdddedce641760d55
capotan <afakan@yandex.com>
2013-10-06 14:43:47 +0300

    Delete banner.php

--- banner.php
+++
@@ -1,4 +0,0 @@
-		<div id="bannerContainer">
-		<div class="bannerAdi">FOLLOWENT </div>
-		<div class="bannerLinkler"><div class="bannerLink">Etkinlikler</div><div class="bannerLink">Gruplar</div><div class="bannerLink">Diğer</div> Giriş Yap</div>
-		</div>
